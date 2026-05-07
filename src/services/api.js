775714c2c9b8260a7ed1b2bcbdb46f1fc92d2573
Jafet San <j.s.b_95@outlook.com>
2026-05-07 10:11:39 -0600

    KAN-15: Creación de plantilla base apiService para la conexión a BD

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://localhost:5173/api'; //Definición de la URL base para las solicitudes a la API
+
+// Función para evaluar la respuesta del servidor y manejar errores
+const apiService = {
+    handleResponse: (response) => {
+        if (!response.ok) {
+            throw new Error(`Error en la petición: ${response.status} ${response.statusText1}`);
+        }
+        return await response.json();
+    }
+    //Aquí se agregarán las funciones y métodos para los siguientes tickets, KAN-16 y KAN-17
+
+};
+export default apiService;
